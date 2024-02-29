ab7368d8f7783de7a85e0e8e7383d1939d16f18d
Chris Booty <chrisalan313@gmail.com>
2024-02-28 20:41:09 -0500

    Finish chapter 3 exercises

---
+++ chapter-3/bean-counting.js
@@ -0,0 +1,54 @@
+'use strict'
+// Exercise: Write a function that counts the number of times 'B' appears in
+// a string. Then write a more general function that counts the number of any
+// given letter in a string.
+
+// I'm going to change this exercise and do it backwards. I'm going to write
+// a third function first which returns a function and then use that for both.
+
+/**
+ * countBs takes a string and returns a number which is the number of times
+ * uppercase B is used in the string.
+ *
+ * @param {string} string - This is the string that will be searched for B's.
+ * @returns {number} This is the count of B's within that string.
+ */
+const countBs = createCharCounter('B');
+
+/**
+ * countChar takes a string and a character and returns the number of times the
+ * letter appears within the given string. Note: The character is case
+ * sensitive.
+ *
+ * @param {string} string - This is the string that will be searched.
+ * @param {string} char - This is the character which will be searched for.
+ * @returns {number} This is the count of the given character in the given
+ * string.
+ */
+function countChar(string, char) {
+  return createCharCounter(char)(string)
+}
+
+/**
+ * createCharCounter takes a single character as an argument and returns a
+ * function which takes a string and returns a number which is the count of the
+ * given character in the given string.
+ *
+ * @param {string} char - This is the string which the returned function will
+ * search the string passed to it for.
+ * @returns {function} This returned function takes a string and returns a
+ * number.
+ */
+function createCharCounter(char) {
+  return (string) => {
+    let result = 0;
+    for (let i = 0; i < string.length; i++) {
+      if (string[i] === char) result++;
+    }
+    return result;
+  }
+}
+
+console.log(createCharCounter('B')('BBC'));
+console.log(countBs('BBC'));
+console.log(countChar('kakkerlak', 'k'))
---
+++ chapter-3/recursion.js
@@ -0,0 +1,35 @@
+'use strict'
+// Exercise: Define a recursive function isEven which takes a positive whole
+// number and returns true if even or false if odd. The book outlines in more
+// detail specifics, which make the exercise more interesting.
+
+/**
+  * isEven takes a positive, whole number and returns true if the number is even or false if
+  * the number is odd.
+  *
+  * @param {number} number - Any positive, whole number.
+  */
+function isEven(number) {
+  // If less than 0, return false. Less than 0 rather than exactly -1 guards
+  // against some case where we go past -1.
+  if (number < 0) return false;
+  // If 0, return true
+  if (number == 0) return true;
+  // If none of the above, default to calling isEven(number - 2). Eventually,
+  // we get to 0 if it's an even number or less than 0 if it isn't even.
+  return isEven(number - 2)
+}
+
+console.log('"Invalid" cases')
+console.log('With -1', isEven(-1))
+console.log('With Math.PI', isEven(Math.PI))
+
+console.log('\n"Valid" odd cases')
+console.log('With 1', isEven(1))
+console.log('With 75', isEven(75))
+console.log('With 1533', isEven(1533))
+
+console.log('\n"Valid" even cases')
+console.log('With 2', isEven(2))
+console.log('With 50', isEven(50))
+console.log('With 452', isEven(452))
